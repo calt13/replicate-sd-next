04fa412e123ec5217f4e9e7559e104448ee078d0
Caltara <calt13@proton.me>
2023-04-23 18:17:08 +0800

    feat: req.body as input

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -2,11 +2,11 @@ export default async function handler(req, res) {
   const authHeader = req.headers.authorization;
   const validAccessCode = process.env.ACCESS_CODE;
 
-  if (!authHeader || authHeader !== `Bearer ${validAccessCode}`) {
-    res.statusCode = 401;
-    res.end(JSON.stringify({ message: "Unauthorized" }));
-    return;
-  }
+  // if (!authHeader || authHeader !== `Bearer ${validAccessCode}`) {
+  //   res.statusCode = 401;
+  //   res.end(JSON.stringify({ message: "Unauthorized" }));
+  //   return;
+  // }
 
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
       // 1.5  328bd9692d29d6781034e3acab8cf3fcb122161e6f5afb896a4ca9fd57090577
       // 2.1 https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf
       // This is the text prompt that will be submitted by a form on the frontend
-      input: { prompt: req.body.prompt },
+      input: req.body,
     }),
   });
 
